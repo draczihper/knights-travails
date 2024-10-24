dfe52c7e781576091739dacbbd5b2b0761d313ac
Drac Zihper <draczihper@gmail.com>
2024-10-24 20:30:19 +0300

    Add knight moves variable

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ function isValid(x, y) {
     return x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE;
 }
 
+const knightMoves = [
+    [2, 1], [1, 2], [2, -1], [-2, 1], [-2, -1], [-1, -2], [-1, 2], [1, -2]
+]
