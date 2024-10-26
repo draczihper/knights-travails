5e83fbdfe62b3100a99685d63eef69f807217c8a
Drac Zihper <draczihper@gmail.com>
2024-10-26 19:59:03 +0300

    Add knightMoves function

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ function isValid(x, y) {
     return x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE;
 }
 
-const knightMoves = [
+const allowedKnightMoves = [
     [2, 1], [1, 2], [2, -1], [-2, 1], [-2, -1], [-1, -2], [-1, 2], [1, -2]
 ];
+
+function knightMoves(start, end) {
+    const [startX, startY] = start;
+    const [endX, endY] = end;
+
+    if (startX === endX && startY === endY) {
+        return [start];
+    }
+}
 
 
 class chessBoardGraph {
