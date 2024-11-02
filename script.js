204ecd4a902b9b8bb63a1d34ea2d76b407d821ee
Drac Zihper <draczihper@gmail.com>
2024-11-02 17:27:42 +0300

    Format document

--- script.js
+++ script.js
@@ -16,37 +16,46 @@ const allowedKnightMoves = [
 ];
 
 function knightMoves(start, end) {
-  const [startX, startY] = start;
-  const [endX, endY] = end;
+    const [startX, startY] = start;
+    const [endX, endY] = end;
 
-  if (startX === endX && startY === endY) {
-    return [start];
-  }
-}
-
-const queue = [[start]];
-
-const visited = new Set();
-visited.add(`${startX},${startY}`);
-
-while (queue.length > 0) {
-  const path = queue.shift();
-
-  const [currentX, currentY] = path[path.length - 1];
-
-  for (let [dx, dy] of allowedKnightMoves) {
-    const newX = currentX + dx;
-    const newY = currentY + dy;
-
-    if (isValid(newX, newY) && !visited.has(`${newX},${newY}`)) {
-      const newPath = [...path, [newX, newY]];
-
-      if (newX === endX && newY === endY) {
-        return newPath;
-      }
-
-      queue.push(newPath);
-      visited.add(`${newX},${newY}`);
+    if (startX === endX && startY === endY) {
+        return {moves: 0, path:[start]};
     }
-  }
+
+
+    const queue = [{ path: [start], moves:0 }];
+
+    const visited = new Set();
+    visited.add(`${startX},${startY}`);
+
+    while (queue.length > 0) {
+        const { path, moves } = queue.shift();
+
+        const [currentX, currentY] = path[path.length - 1];
+
+        for (let [dx, dy] of allowedKnightMoves) {
+            const newX = currentX + dx;
+            const newY = currentY + dy;
+
+            if (isValid(newX, newY) && !visited.has(`${newX},${newY}`)) {
+                const newPath = [...path, [newX, newY]];
+
+                if (newX === endX && newY === endY) {
+                    return { moves: moves + 1, path: newPath };
+                }
+
+                queue.push({ path: newPath, moves: moves + 1});
+                visited.add(`${newX},${newY}`);
+            }
+        }
+    }
+    
+    return {moves: -1, path: []};
 }
+
+
+const result = knightMoves([0, 0], [7, 7]);
+
+console.log(`You made it in ${result.moves} moves! Here is your path: `);
+console.log(result.path);
